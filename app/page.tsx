e576fae793b85c1c42021d79d57fe7610f72030c
CailynMuljadi <cailynmuljadi@gmail.com>
2026-07-13 21:55:35 +0700

    shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button"
+
 const Home=() => {
   return <div className="p-10 bg-shop-light-pink">
-    <h2 className="text-2xl font-semibold">Home</h2>
+    <h2 className="text-2xl font-semibold font-poppins">Welcome to our store!</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quibusdam cum, 
       maxime, nihil recusandae atque ex aliquid odit adipisci, necessitatibus commodi. 
@@ -8,6 +10,9 @@ const Home=() => {
       voluptatum necessitatibus aperiam a quod, maiores ipsum excepturi voluptatibus quia, 
       sequi optio. Pariatur in magnam nihil nostrum modi nulla repudiandae.
       </p>
+      <Button size="lg" variant="destructive">
+        Check Out
+      </Button>
     </div>
 };
 
